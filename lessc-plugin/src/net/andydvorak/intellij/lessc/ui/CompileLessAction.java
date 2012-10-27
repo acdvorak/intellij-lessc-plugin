486c190184ba5e676c2711258c723dce184deb0e
Andrew C. Dvorak <acdvorak@gmail.com>
2012-10-26 21:56:15 -0500

    fixes #2 : Add "Compile to CSS" Action to context menu for .less files

---
+++ lessc-plugin/src/net/andydvorak/intellij/lessc/ui/CompileLessAction.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2012 Andrew C. Dvorak.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.andydvorak.intellij.lessc.ui;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileEvent;
+import net.andydvorak.intellij.lessc.LessManager;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author Andrew C. Dvorak
+ * @since 10/26/12
+ */
+public class CompileLessAction extends AnAction {
+
+    public void actionPerformed(AnActionEvent _e) {
+        final AnActionEventWrapper e = new AnActionEventWrapper(_e);
+        final Collection<VirtualFile> files = e.getLessFiles();
+
+        for (VirtualFile file : files) {
+            final VirtualFileEvent virtualFileEvent = new VirtualFileEvent(this, file, file.getName(), file.getParent());
+            LessManager.getInstance(e.getProject()).handleFileEvent(virtualFileEvent);
+        }
+    }
+
+    /**
+     * @see <a href="http://devnet.jetbrains.net/message/5126605#5126605">JetBrains forum post</a>
+     */
+    public void update(AnActionEvent _e) {
+        super.update(_e);
+
+        final AnActionEventWrapper e = new AnActionEventWrapper(_e);
+
+        boolean visible = e.hasProject() && e.hasLessFiles();
+
+        // Visibility
+        e.getPresentation().setVisible(visible);
+
+        // Enable or disable
+        e.getPresentation().setEnabled(visible);
+    }
+
+    private static class AnActionEventWrapper extends AnActionEvent {
+        public AnActionEventWrapper(final AnActionEvent e) {
+            super(e.getInputEvent(), e.getDataContext(), e.getPlace(), e.getPresentation(), e.getActionManager(), e.getModifiers());
+        }
+
+        public VirtualFile getVirtualFile() {
+            return getData(PlatformDataKeys.VIRTUAL_FILE);
+        }
+
+        public VirtualFile[] getVirtualFiles() {
+            return getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
+        }
+
+        public boolean hasProject() {
+            final Project project = getProject();
+            return project != null && !project.isDisposed();
+        }
+
+        public boolean hasLessFiles() {
+            return hasLessFiles(getVirtualFiles());
+        }
+
+        public Collection<VirtualFile> getLessFiles() {
+            return getLessFiles(getVirtualFiles());
+        }
+
+        public static boolean hasLessFiles(final VirtualFile[] files) {
+            if (files == null || files.length == 0) return false;
+
+            for (VirtualFile file : files) {
+                if ("LESS".equals(file.getFileType().getName()))
+                    return true;
+            }
+
+            // Search subdirectories recursively
+            for (VirtualFile file : files) {
+                if (hasLessFiles(file.getChildren()))
+                    return true;
+            }
+
+            return false;
+        }
+
+        /**
+         * Traverses an array of VirtualFiles recursively and returns all LESS files in the array and subdirectories
+         * of folders in the array.
+         * @param files array of VirtualFiles (files and/or folders) selected by the user which may or may not contain LESS files
+         * @return Collection of LESS files
+         */
+        public static Collection<VirtualFile> getLessFiles(final VirtualFile[] files) {
+            final ArrayList<VirtualFile> lessFiles = new ArrayList<VirtualFile>();
+
+            if (ArrayUtils.isEmpty(files))
+                return lessFiles;
+
+            for (VirtualFile file : files) {
+                if ("LESS".equals(file.getFileType().getName()))
+                    lessFiles.add(file);
+
+                lessFiles.addAll(getLessFiles(file.getChildren()));
+            }
+
+            return lessFiles;
+        }
+    }
+
+}
