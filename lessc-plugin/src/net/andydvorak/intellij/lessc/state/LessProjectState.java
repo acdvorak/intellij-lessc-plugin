acdvorak/intellij-lessc-plugin

commit 153d75901ef1d9b86d64a2ca5d53551eafae0e81

Fixed some inspection warnings

lessc-plugin/src/net/andydvorak/intellij/lessc/state/LessProjectState.java
@@ -20,7 +20,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LessProjectState {
-    public Map<String, LessProfile> lessProfiles = new LinkedHashMap<String, LessProfile>();
+    public final Map<String, LessProfile> lessProfiles = new LinkedHashMap<String, LessProfile>();
 
     @Override
     public boolean equals(Object o) {
@@ -29,9 +29,7 @@ public class LessProjectState {
 
         LessProjectState that = (LessProjectState) o;
 
-        if (lessProfiles != null ? !lessProfiles.equals(that.lessProfiles) : that.lessProfiles != null) return false;
-
-        return true;
+        return !(lessProfiles != null ? !lessProfiles.equals(that.lessProfiles) : that.lessProfiles != null);
     }
 
     @Override

lessc-plugin/src/net/andydvorak/intellij/lessc/ui/LessProfilesPanel.java
@@ -77,7 +77,7 @@ public class LessProfilesPanel extends MasterDetailsComponent implements Searcha
     }
 
     protected boolean wasObjectStored(Object o) {
-        return lessManager.getProfiles().contains(o);
+        return o instanceof LessProfile && lessManager.getProfiles().contains(o);
     }
 
     @Nls

lessc-plugin/src/net/andydvorak/intellij/lessc/LessManager.java
@@ -17,13 +17,7 @@
 package net.andydvorak.intellij.lessc;
 
 import com.asual.lesscss.LessException;
-import com.intellij.compiler.impl.CompileContextImpl;
-import com.intellij.compiler.impl.ProjectCompileScope;
-import com.intellij.compiler.progress.CompilerTask;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.compiler.CompileContext;
-import com.intellij.openapi.compiler.CompileScope;
-import com.intellij.openapi.compiler.CompileTask;
 import com.intellij.openapi.components.*;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicator;
@@ -51,6 +45,7 @@ import net.andydvorak.intellij.lessc.notification.Notifier;
 import net.andydvorak.intellij.lessc.state.CssDirectory;
 import net.andydvorak.intellij.lessc.state.LessProfile;
 import net.andydvorak.intellij.lessc.state.LessProjectState;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
@@ -185,7 +180,7 @@ public class LessManager extends AbstractProjectComponent implements PersistentS
         final File lessProfileDir = lessCompileJob.getLessProfile().getLessDir();
         final File lessFile = lessCompileJob.getLessFile().getCanonicalFile();
 
-        final String relativeLessPath = FileUtil.getRelativePath(lessProfileDir, lessFile);
+        final String relativeLessPath = StringUtils.defaultString(FileUtil.getRelativePath(lessProfileDir, lessFile));
         final String relativeCssPath = relativeLessPath.replaceFirst("\\.less$", ".css");
 
         final String cssTempFileContent = FileUtil.loadFile(lessCompileJob.getCssTempFile());
@@ -217,7 +212,7 @@ public class LessManager extends AbstractProjectComponent implements PersistentS
         return numUpdated > 0;
     }
 
-    private void compileImporters(final LessCompileJob lessCompileJob) throws IOException, LessException {
+    private void compileImporters(final LessCompileJob lessCompileJob) throws IOException {
         final Set<String> importerPaths = LessFile.getImporterPaths(lessCompileJob.getLessFile(), lessCompileJob.getLessProfile());
 
         for ( String importerPath : importerPaths ) {
@@ -278,32 +273,6 @@ public class LessManager extends AbstractProjectComponent implements PersistentS
                 }
             });
         }
-    }
-
-    private void compileWithNotifier(final VirtualFileEvent virtualFileEvent, final Runnable onTaskFinished) {
-        final String sourceFilePath = virtualFileEvent.getFile().getCanonicalPath();
-        final CompilerTask compilerTask = new CompilerTask(myProject, true, sourceFilePath, false);
-        final CompileScope compileScope = new ProjectCompileScope(myProject);
-        final CompileContext compileContext = new CompileContextImpl(myProject, compilerTask, compileScope, null, false, false);
-
-        final CompileTask task;
-/*
-        compilerTask.start(new Runnable() {
-            public void run() {
-                try {
-                    task.execute(compileContext);
-                }
-                catch (ProcessCanceledException ex) {
-                    // suppressed
-                }
-                finally {
-                    if (onTaskFinished != null) {
-                        onTaskFinished.run();
-                    }
-                }
-            }
-        }, null);
-*/
     }
 
     public void handleFileEvent(final VirtualFileMoveEvent virtualFileMoveEvent) {

lessc-plugin/src/net/andydvorak/intellij/lessc/ui/CssConfigurableForm.java
@@ -38,8 +38,10 @@ import com.intellij.util.ui.ListTableModel;
 import net.andydvorak.intellij.lessc.LessManager;
 import net.andydvorak.intellij.lessc.state.CssDirectory;
 import net.andydvorak.intellij.lessc.state.LessProfile;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
@@ -213,8 +215,10 @@ public class CssConfigurableForm extends NamedConfigurable<LessProfile> {
 
     @Nullable
     private String promptForFilePath(final @Nullable String initial) {
+        @NotNull
+        final String initialNN = StringUtils.defaultString(initial);
         final FileChooserDescriptor d = getFileChooserDescriptor();
-        final VirtualFile initialFile = StringUtil.isNotEmpty(initial) ? LocalFileSystem.getInstance().findFileByPath(initial) : null;
+        final VirtualFile initialFile = StringUtil.isNotEmpty(initialNN) ? LocalFileSystem.getInstance().findFileByPath(initialNN) : null;
         final VirtualFile file = project != null ? FileChooser.chooseFile(project, d, initialFile) : FileChooser.chooseFile(profileMappingTable, d, initialFile);
         String path = null;
         if (file != null) {

lessc-plugin/src/net/andydvorak/intellij/lessc/ui/TextFieldWithBrowseButtonListener.java
@@ -24,6 +24,7 @@ import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -37,7 +38,7 @@ public class TextFieldWithBrowseButtonListener extends TextFieldWithBrowseButton
         this(project, null);
     }
 
-    public TextFieldWithBrowseButtonListener(final Project project, final String title) {
+    public TextFieldWithBrowseButtonListener(@Nullable final Project project, @Nullable final String title) {
         super();
 
         this.myProject = project;

commit 6c748fd9dd173af863d8a87a917c79111350ee7b

Fixed regression from commit 153d759 / d2e932d (preferences were being deleted)

lessc-plugin/src/net/andydvorak/intellij/lessc/state/LessProjectState.java
@@ -20,7 +20,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LessProjectState {
-    public final Map<String, LessProfile> lessProfiles = new LinkedHashMap<String, LessProfile>();
+    public Map<String, LessProfile> lessProfiles = new LinkedHashMap<String, LessProfile>();
 
     @Override
     public boolean equals(Object o) {
@@ -29,7 +29,9 @@ public class LessProjectState {
 
         LessProjectState that = (LessProjectState) o;
 
-        return !(lessProfiles != null ? !lessProfiles.equals(that.lessProfiles) : that.lessProfiles != null);
+        if (lessProfiles != null ? !lessProfiles.equals(that.lessProfiles) : that.lessProfiles != null) return false;
+
+        return true;
     }
 
     @Override

lessc-plugin/src/net/andydvorak/intellij/lessc/ui/LessProfilesPanel.java
@@ -77,7 +77,7 @@ public class LessProfilesPanel extends MasterDetailsComponent implements Searcha
     }
 
     protected boolean wasObjectStored(Object o) {
-        return o instanceof LessProfile && lessManager.getProfiles().contains(o);
+        return lessManager.getProfiles().contains(o);
     }
 
     @Nls